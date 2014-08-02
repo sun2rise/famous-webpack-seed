25e8a274f504fcdeb2efb24c8eac302cb4536b8a
Mark Marijnissen <markmarijnissen@gmail.com>
2014-08-02 23:59:43 +0200

    + live-reload without iframe with the --sync option + build multiple apps (one directory per app) + extra options: --minify (without mangling), --dir (change output dir), --app (build single directory) --target options sets global TARGET variable

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 #Famous-Webpack-Seed
 > A seed project to get started with Webpack and Famo.us
 
+###Features
+
+* Multiple bundles: Every directory in `./src/` is bundled as an app.
+* Require Jade (.jade), LESS (`.less`) and Coffee (`.coffee`)
+* Use `--sync` to use the webpack-dev-server without the iframe! 
+* Use `--target=xxx` to set a global TARGET variable (useful for multiple configurations)
+* Use `--minify` to minify the bundle (without mangle)
+* Use `--dir=yyy` to change the output directory
+* Use `--app=zzz` to only build a single directory (e.g. `--app=boilerplate` builds `./src/boilerplate/main.js`)
+
 ---
 
 ###Installation
@@ -17,9 +27,15 @@ npm install # install dependencies
 ###Development
 Run the dev server with ```webpack-dev-server```
 
-Go to the dev server url : ```http://localhost:8080/webpack-dev-server/bundle```
+Go to the dev server url : ```http://localhost:8080/webpack-dev-server/boilerplate```. This is live-reloaded with an iframe.
+
+When using ```webpack-dev-server --sync```, the normal build (```http://localhost:8080/boilerplate```) will also live reload (without iframe).
 
 ---
 
 ###Production
-Build the deployable static assets with ```webpack```
+Build the deployable static assets with ```webpack --minify```
+
+(Note/todo: webpack also provides a `-p` option, but this mangles output. Not sure how to modify UglifyJS options when using the `-p` option)
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,92 @@
-module.exports = {
-  cache: true,
-  context: __dirname,
-  entry: './src/index.js',
-  output: {
-    path: __dirname + '/dist/',
-    filename: 'bundle.js',
-    publicPath: "/dist/"
+var webpack = require('webpack');
+var path = require('path');
+
+var options = getOptions();
+
+var config = {
+  "context": path.join(__dirname,'src'),
+  "output":{
+    path:options.dir,
+    filename:"[name].js"
   },
-  module: {
-    loaders: [
-      { test: /\.css$/,    loader: 'style!css' },
-      { test: /\.png$/,    loader: 'file?prefix=images/' }
+  "entry":options.entries,
+  "module":{
+    "loaders":[
+      { test: /\.json$/,   loader: "json-loader" },
+      { test: /\.coffee$/, loader: "coffee-loader" },
+      { test: /\.css$/,    loader: "style-loader!css-loader" },
+      { test: /\.less$/,   loader: "style-loader!css-loader!less-loader" },
+      { test: /\.jade$/,   loader: "jade-loader" },
+      { test: /\.(png|jpg|gif)$/,    loader: "url-loader?limit=5000" },
+      { test: /\.eot$/,    loader: "file-loader" },
+      { test: /\.ttf$/,    loader: "file-loader" },
+      { test: /\.svg$/,    loader: "file-loader" },
+      { test: /const(ants)?\.js$/, loader: "expose?CONST" },
+      
     ]
-  }
+  },
+  "plugins":[
+    new webpack.DefinePlugin({
+      VERSION: JSON.stringify(require('./package.json').version),
+      TARGET: JSON.stringify(options.t)
+    })
+  ]
 };
+
+if(options.d){
+  config.output.path = options.d;
+}
+if(options.m){
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin({mangle:false}));
+}
+
+module.exports = config;
+
+function getOptions(){
+  opt = require('optimist')
+    .alias('a','app')
+    .alias('t','target')
+    .default('t','dev')
+    .alias('m','minify')
+    .default('m',false)
+    .alias('d','dir')
+    .default('d','dist')
+    .alias('h','help')
+    .alias('s','sync')
+    .default('s',false)
+    .argv;
+
+  if(opt.help){
+    console.log(
+      "Webpack builds every 'main.js' in /src/[bundle]/\n"+
+      "Every directory in /src/ is bundled as a seperate app.\n\n"+
+      "Extra options:\n"+
+      "\t-t, --target\tset a global TARGET variable (default: 'dev')\n"+
+      "\t-m, --minify\tminify (without mangle) (default: false)\n"+
+      "\t-a, --app\tbuild a single folder (default: all)\n"+
+      "\t-d, --dir\tset output directory (default 'dist')\n"
+      );
+  }
+
+  var apps;
+  if(opt.a) {
+    apps = ['src/'+opt.a+'/main.js'];
+  } else {
+    apps = require('glob').sync(path.join('src','!(node_modules)','main.js'));
+  }
+
+  var sync = "webpack-dev-server/client?http://localhost:8080/";
+
+  var entries = {};
+  apps.forEach(function(app){
+    // voodoo magic
+    var entry = "./" + app.substr(4,app.length-7);
+    var name = entry.substr(2,entry.length-7).replace(/(\/|\\)/g,'-');
+    // app = ./src/app/main
+    entries[name] = opt.s? [sync , entry] :[entry];
+  });
+
+  opt.entries = entries;
+  console.log('TARGET='+opt.t+' APP='+(opt.a?opt.a:'all')+' SYNC='+opt.s+' MINIFY='+opt.m+' DIR='+opt.d);
+  return opt;
+}
