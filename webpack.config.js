e20ed77bb0eabe67afd1a136ee9197397524bbab
talves <tony@3alves.com>
2014-08-17 18:33:11 -0700

    restructure the absolute paths when entries are in sub directories

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var webpack = require('webpack');
 var ReloadPlugin = require('webpack-reload-plugin');
 var path = require('path');
+var isRunningDevServer = isDevServer();
 
 // Support for extra commandline arguments
 var argv = require('optimist')
@@ -10,12 +11,14 @@ var argv = require('optimist')
             .alias('m','minify')
             .argv;
 
+      console.log(__dirname);
 var config = {
-  entry:getEntries(), // every ./src/XXXX/main.js
+  context: path.join(__dirname, "src"),
+  entry: getEntries(), // every ./src/XXXX/main.js
   output:{
-    path: './dist',
-    filename:"[name]/bundle.js",
-    publicPath: '../'
+    path: path.join(__dirname, "dist"),
+    filename:"[name]/js/bundle.js",
+    publicPath: isRunningDevServer ? '/': ''
   },
   devServer: {
     publicPath: '/'
@@ -28,10 +31,10 @@ var config = {
       { test: /\.css$/,             loader: "style-loader!css-loader" },
       { test: /\.less$/,            loader: "style-loader!css-loader!less-loader" },
       { test: /\.jade$/,            loader: "jade-loader" },
-      { test: /\.(png|jpg|gif)$/,   loader: "url-loader?limit=50000&name=[path][name].[ext]&context=./src" },
-      { test: /\.eot$/,             loader: "file-loader?name=[path][name].[ext]&context=./src" },
-      { test: /\.ttf$/,             loader: "file-loader?name=[path][name].[ext]&context=./src" },
-      { test: /\.svg$/,             loader: "file-loader?name=[path][name].[ext]&context=./src" },
+      { test: /\.(png|jpg|gif)$/,   loader: "url-loader?limit=50000" + assetParams() },
+      { test: /\.eot$/,             loader: "file-loader" + assetParams() },
+      { test: /\.ttf$/,             loader: "file-loader" + assetParams() },
+      { test: /\.svg$/,             loader: "file-loader" + assetParams() },
       { test: /index\.html$/,       loader: "file-loader?name=[path][name].[ext]&context=./src" },
       { test: /const(ants)?\.js$/,  loader: "expose?CONST" }
     ]
@@ -54,19 +57,31 @@ if(argv.minify){
  * Search for "/src/xxx/main.js" and return { xxx: './xxx/main' }
  */
 function getEntries(){
-  var apps = require('glob').sync(path.join('src','!(node_modules)','main.js'));
+  // Get all entry points of our application starting with the root
+  // Our bundles will be placed in a relative path ./js
+  var apps = require('glob').sync(path.join('./src/**/main.js'));
   var entries = {};
   apps.forEach(function(file){
-    //                                             file =   src/boilerplate/main.js
-    var entry = "./" + file.substr(0,file.length-3); // = ./src/boilerplate/main
-    var name = entry.substr(6,entry.length-11);      // =       boilerplate
+    // Example file = src/boilerplate/main.js
+    var entry = "./" + file.substr(4,file.length-7); // = ./boilerplate/main
+    var partsOfFile = file.split('/'); // array of path parts
+    var name = '';
+    partsOfFile.forEach(function(part){ name += (part !== 'src' && part !== 'main.js') ? (name === '')?  part : '/' + part : ''});
+    if (name === '') name = 'assets'; // [optional] setup for a root entry. point to assets/js/bundle.js in index.html
     entries[name] = entry;
   });
-  return entries;
+   return entries;
 }
 
 function isDevServer(){
   return process.argv.join('').indexOf('webpack-dev-server') > -1;
+}
+
+function assetParams() {
+    // we want a relative path for our entry points, we will set the context in our sub directory images
+    // see /boilerplate/main.js for an example.
+    return isRunningDevServer? '?name=[path][name].[ext]&context=./src' : '?name=[path][name].[ext]';
+    // if you want absolute paths for your entry points, change the output.publicPath value from '' to '/'
 }
 
 module.exports = config;
